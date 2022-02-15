933e434f058bfe47451bc2825f91c4874237805c
kajshar <60972901+kajshar@users.noreply.github.com>
2022-02-15 17:16:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 from flask import Flask
-from flask import jsonify
-import config as config
-
 app = Flask(__name__)
-
 @app.route('/')
-def hello():    
-    return 'Welcome to the Application with updated code!'
-
-@app.route('/stub')
-def stub():
-    # Stub value replaced by Jenkins Pipeline dynamically in config.py
-    return 'Value of Stub: ' + str(config.STUB_VARIABLE)
-    
+def hello_world():
+    return "Hello World!"
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,host='0.0.0.0')
